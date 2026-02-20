5c6840e3a43dd59d960fe249a5247d1cc886f2bc
fezcode <samil.bulbul@gmail.com>
2026-02-20 23:23:56 +0300

    feat: add version flag support

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 	"atlas.otp/internal/ui"
 )
 
+var Version = "0.1.0"
+
 func main() {
+	if len(os.Args) > 1 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Printf("atlas.otp v%s\n", Version)
+		return
+	}
+
 	store, err := storage.NewStore()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error initializing store: %v\n", err)
