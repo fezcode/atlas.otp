3f7c23761dd099e99923967cda5c64a7b8a91add
fezcode <samil.bulbul@gmail.com>
2026-03-04 00:48:46 +0300

    Bump v0.1.2: add --help flag

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ func main() {
 		fmt.Printf("atlas.otp v%s\n", Version)
 		return
 	}
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help" || os.Args[1] == "help") {
+		fmt.Println("Atlas OTP - Minimalist, secure terminal TOTP (2FA) manager.")
+		fmt.Println("\nUsage:")
+		fmt.Println("  atlas.otp        Start the TOTP manager TUI")
+		fmt.Println("  atlas.otp -v     Show version")
+		fmt.Println("  atlas.otp -h     Show this help")
+		return
+	}
 
 	store, err := storage.NewStore()
 	if err != nil {
